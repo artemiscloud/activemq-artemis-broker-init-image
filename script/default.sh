40d758c00c20c69054919e009e7151ee0ffb05e5
borcsokjozsef <borcsok.jozsef@alerant.hu>
2022-10-26 14:36:50 +0200

    [#25] support writing post-config.sh by environment variable

--- script/default.sh
+++ script/default.sh
@@ -5,6 +5,13 @@
 #configuration. It not do nothing.
 
 CUSTOM_CFG=/amq/scripts/post-config.sh
+if [ "${POST_CONFIG}" != "" ]
+then
+    echo "Writing environment variable POST_CONFIG to ${CUSTOM_CFG}"
+    mkdir -p "$(dirname "${CUSTOM_CFG}")"
+    echo "${POST_CONFIG}" > "${CUSTOM_CFG}"
+    chmod +x "${CUSTOM_CFG}"
+fi
 echo "exposing env var CONFIG_INSTANCE_DIR for custom init"
 echo "CONFIG_INSTANCE_DIR value from ${CONFIG_INSTANCE_DIR}"
 export CONFIG_INSTANCE_DIR=${CONFIG_INSTANCE_DIR}/${AMQ_NAME}

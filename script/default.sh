7131282ae407b226af50bd3b64e78aadc40cdeeb
borcsokjozsef <borcsok.jozsef@alerant.hu>
2022-11-07 16:26:19 +0100

    [#25] support overriding CUSTOM_CFG (so post-config.sh can be added by operator extraMount)

--- script/default.sh
+++ script/default.sh
@@ -4,14 +4,7 @@
 #script. If it exists it calls it for
 #configuration. It not do nothing.
 
-CUSTOM_CFG=/amq/scripts/post-config.sh
-if [ "${POST_CONFIG}" != "" ]
-then
-    echo "Writing environment variable POST_CONFIG to ${CUSTOM_CFG}"
-    mkdir -p "$(dirname "${CUSTOM_CFG}")"
-    echo "${POST_CONFIG}" > "${CUSTOM_CFG}"
-    chmod +x "${CUSTOM_CFG}"
-fi
+CUSTOM_CFG=${CUSTOM_CFG:-/amq/scripts/post-config.sh}
 echo "exposing env var CONFIG_INSTANCE_DIR for custom init"
 echo "CONFIG_INSTANCE_DIR value from ${CONFIG_INSTANCE_DIR}"
 export CONFIG_INSTANCE_DIR=${CONFIG_INSTANCE_DIR}/${AMQ_NAME}
@@ -20,5 +13,5 @@ echo "Finding default custom script at ${CUSTOM_CFG}"
 if [ -f "${CUSTOM_CFG}" ]; then
     echo "Found custom script ${CUSTOM_CFG}, executing it"
     ls ${CUSTOM_CFG}
-    ${CUSTOM_CFG}
+    /bin/bash ${CUSTOM_CFG}
 fi
